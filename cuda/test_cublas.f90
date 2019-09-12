31a6ab43255657257bf024570f4c4d8029af621f
tkdhss111 <tkdhss111@gmail.com>
2019-09-12 23:36:06 +0900

    cuda matmul

--- cuda/test_cublas.f90
+++ cuda/test_cublas.f90
@@ -1,5 +1,6 @@
-module mod_gemm
+module mod_cublas_sgemm
 
+  use iso_fortran_env, only: rk => real32
   use cudafor
   use cublas
 
@@ -7,64 +8,67 @@ module mod_gemm
 
   private
 
-  public :: gemm
+  public :: cublas_sgemm, matmul
 
   contains
 
-    subroutine gemm (a, b, c, transa, transb, alpha, beta)
+    subroutine cublas_sgemm (a, b, c, transa, transb, alpha, beta)
 
-      real(4),      intent(in)           :: a(:, :), b(:, :)
-      real(4),      intent(inout)        :: c(:, :)
-      real(4),      intent(in), optional :: alpha, beta
-      real(4)                            :: o_alpha, o_beta
+      ! Input/output variables
+      real(rk),     intent(in)           :: a(:, :), b(:, :)
+      real(rk),     intent(inout)        :: c(:, :)
+      real(rk),     intent(in), optional :: alpha, beta
       character(1), intent(in), optional :: transa, transb
-      integer                            :: m, n, k, lda, ldb, ldc
 
-      ! GPU
-      type(cublasHandle)           :: handle
-      real(4), device, allocatable :: dA(:, :), dB(:, :), dC(:, :)
-      integer                      :: itransa, itransb
-      integer                      :: ierr, istat
+      ! Local variables
+      real(rk) :: o_alpha, o_beta
+      integer  :: m, n, k, lda, ldb, ldc
+
+      ! GPU variables
+      type(cublasHandle)            :: handle
+      real(rk), device, allocatable :: dA(:, :), dB(:, :), dC(:, :)
+      integer                       :: itransa, itransb
+      integer                       :: ierr, istat
 
       intrinsic max, present, size
 
-      IF(PRESENT(ALPHA)) THEN
-          O_ALPHA = ALPHA
-      ELSE
-          O_ALPHA = 1
-      ENDIF
+      if ( present(alpha) ) then
+        o_alpha = alpha
+      else
+        o_alpha = 1
+      endif
 
-      IF(PRESENT(BETA)) THEN
-          O_BETA = BETA
-      ELSE
-          O_BETA = 0
-      ENDIF
+      if ( present(beta) ) then
+        o_beta = beta
+      else
+        o_beta = 0
+      endif
 
-      IF(PRESENT(TRANSA)) THEN
-        if (transa == 'N' .or. transa == 'n') then
+      if ( present(transa) ) then
+        if ( transa == 'N' .or. transa == 'n' ) then
           itransa = 0
         else
           itransa = 1
         end if
-      ELSE
+      else
           itransa = 0
-      ENDIF
+      endif
 
-      IF(PRESENT(TRANSB)) THEN
-        if (transb == 'N' .or. transb == 'n') then
+      if ( present(transb) ) then
+        if ( transb == 'N' .or. transb == 'n' ) then
           itransb = 0
         else
           itransb = 1
         end if
-      ELSE
+      else
           itransb = 0
-      ENDIF
+      endif
 
-      IF(itransa == 0) THEN
-          K = SIZE(A,2)
-      ELSE
-          K = SIZE(A,1)
-      ENDIF
+      if ( itransa == 0 ) then
+          k = size(a, 2)
+      else
+          k = size(a, 1)
+      endif
 
       lda = max( 1, size(a, 1) )
       ldb = max( 1, size(b, 1) )
@@ -88,29 +92,44 @@ module mod_gemm
 
     end subroutine
 
+    function matmul (a, b) result (c)
+
+      real(rk), intent(in)  :: a(:, :), b(:, :)
+      real(rk), allocatable :: c(:, :)
+
+      allocate ( c(size(a, 1), size(b, 2)) )
+
+      call cublas_sgemm (a, b, c, 'N', 'N', 1.0e0, 0.0e0)
+
+    end function
+
 end module
 
 program test_cublas
 
-  use mod_gemm
+  use iso_fortran_env, only: rk => real32
+  use mod_cublas_sgemm
 
   implicit none
 
-  real(4), allocatable :: a(:, :), b(:, :), c(:, :)
-  integer i, j, n
+  real(rk), allocatable :: a(:, :), b(:, :), c(:, :)
+  integer i, j, m, n, k
 
-  n = 8000
+  m = 8000
+  n = 6000
+  k = 5000
 
-  allocate ( a(n, n), source = 1.0e0 )
-  allocate ( b(n, n), source = 2.0e0 )
-  allocate ( c(n, n), source = 3.0e0 )
+  allocate ( a(m, k), source = 1.0e0 )
+  allocate ( b(k, n), source = 2.0e0 )
+  !allocate ( c(m, n), source = 3.0e0 )
 
-  call gemm (a, b, c, 'N', 'N', 1.0e0, 0.0e0)
+  !call cublas_sgemm (a, b, c, 'N', 'N', 1.0e0, 0.0e0)
+  c = matmul(a, b)
 
-  print *, repeat ('=', 80)
+  print *, repeat('=', 80)
   do i = 1, 5
-    print '(*(f16.2))', [(c(i, j), j = 1, 5)]
+    print '(*(f16.2))', [( c(i, j), j = 1, 5 )]
   end do
-  print *, repeat ('=', 80)
+  print *, repeat('=', 80)
 
 end
